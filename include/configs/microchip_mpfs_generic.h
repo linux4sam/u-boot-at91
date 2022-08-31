7702e9c85b4c6862b36913a1737c8fe539d15045
Valentina Fernandez <valentina.fernandezalanis@microchip.com>
2022-08-31 10:43:16 +0100

    icicle: add support for booting from QSPI nand flash memory
    Use the bootcmd_ubifs provided by distro boot. U-boot will look for a
    boot.src boot script in the /boot directory of the rootfs.
    If qspi boot prioritization is set distro boot will try to boot from
    ubifs, followed by mmc and dhcp. If unset it will try to boot from mmc
    followed by dhcp.
    
    Signed-off-by: Valentina Fernandez <valentina.fernandezalanis@microchip.com>

--- include/configs/microchip_mpfs_generic.h
+++ include/configs/microchip_mpfs_generic.h
@@ -13,9 +13,34 @@
 
 /* Environment options */
 
+#if defined(CONFIG_CMD_DHCP)
+#define BOOT_TARGET_DEVICES_DHCP(func)	func(DHCP, dhcp, na)
+#else
+#define BOOT_TARGET_DEVICES_DHCP(func)
+#endif
+
+#if defined(CONFIG_CMD_MMC)
+#define BOOT_TARGET_DEVICES_MMC(func)	func(MMC, mmc, 0)
+#else
+#define BOOT_TARGET_DEVICES_MMC(func)
+#endif
+
+#if defined(CONFIG_CMD_UBIFS)
+#define BOOT_TARGET_DEVICE_UBIFS(func) func(UBIFS, ubifs, 0, ubi, rootfs)
+#else
+#define BOOT_TARGET_DEVICE_UBIFS(func)
+#endif
+
+#if defined(CONFIG_MPFS_PRIORITISE_QSPI_BOOT)
 #define BOOT_TARGET_DEVICES(func) \
-	func(MMC, mmc, 0) \
-	func(DHCP, dhcp, na)
+	BOOT_TARGET_DEVICE_UBIFS(func)	\
+	BOOT_TARGET_DEVICES_MMC(func)\
+	BOOT_TARGET_DEVICES_DHCP(func)
+#else
+#define BOOT_TARGET_DEVICES(func) \
+	BOOT_TARGET_DEVICES_MMC(func)\
+	BOOT_TARGET_DEVICES_DHCP(func)
+#endif
 
 #define BOOTENV_DESIGN_OVERLAYS \
 	"design_overlays=" \
