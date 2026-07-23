linux4microchip/u-boot-mchp

board: sama5d4_xplained: add ft_board_setup()
Add ft_board_setup() where specific device tree changes could be done
before booting the OS. On sama5d4_xplained the specific action added
is the removal of "cd-gpios" property for WILC devices. This property
is not needed by WILC devices and, moreover, some WILC specific
configurations (e.g. ATWILC3000-SHLD) are not even booting if this
property is present.

Signed-off-by: Claudiu Beznea <claudiu.beznea@microchip.com>

board/atmel/sama5d4_xplained/sama5d4_xplained.c
@@ -219,3 +219,31 @@ void at91_pmc_init(void)
 	at91_mck_init(tmp);
 }
 #endif
+
+#if defined(CONFIG_OF_BOARD_SETUP)
+int ft_board_setup(void *fdt, bd_t *bd)
+{
+	static const char * const wilc_compatibles[] = {
+		"microchip,wilc1000", "microchip,wilc3000", "atmel,wilc_sdio",
+	};
+
+	const struct fdt_property *prop;
+	int i, offset, len, ret = 0;
+
+	for (i = 0; i < ARRAY_SIZE(wilc_compatibles); i++) {
+		offset = fdt_node_offset_by_compatible(fdt, -1,
+						       wilc_compatibles[i]);
+		if (offset < 0)
+			continue;
+
+		prop = fdt_get_property(fdt, offset, "cd-gpios", &len);
+		if (!prop)
+			break;
+
+		ret = fdt_delprop(fdt, offset, "cd-gpios");
+		break;
+	}
+
+	return ret;
+}
+#endif

linux4sam/u-boot-at91

[eugen.hristev@microchip.com: port to u-boot 2022.01]
Signed-off-by: Eugen Hristev <eugen.hristev@microchip.com>
(cherry picked from commit b5a31474b3a0cdb689e7ba6ee726b3c3d47d87c9)

board/atmel/sama5d4_xplained/sama5d4_xplained.c
@@ -210,3 +210,31 @@ void at91_pmc_init(void)
 	at91_mck_init(tmp);
 }
 #endif
+
+#if defined(CONFIG_OF_BOARD_SETUP)
+int ft_board_setup(void *fdt, gd_t *gd)
+{
+	static const char * const wilc_compatibles[] = {
+		"microchip,wilc1000", "microchip,wilc3000", "atmel,wilc_sdio",
+	};
+
+	const struct fdt_property *prop;
+	int i, offset, len, ret = 0;
+
+	for (i = 0; i < ARRAY_SIZE(wilc_compatibles); i++) {
+		offset = fdt_node_offset_by_compatible(fdt, -1,
+						       wilc_compatibles[i]);
+		if (offset < 0)
+			continue;
+
+		prop = fdt_get_property(fdt, offset, "cd-gpios", &len);
+		if (!prop)
+			break;
+
+		ret = fdt_delprop(fdt, offset, "cd-gpios");
+		break;
+	}
+
+	return ret;
+}
+#endif